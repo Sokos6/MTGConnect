bfc2828cc3c7d379d589ca5d5372ae3738543f58
Sokos6 <will@comicbooked.com>
2015-10-03 02:17:54 -0400

    added friendships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "sokos6", email: "will@comicbooked.com", password: "laceelou21", password_confirmation: "laceelou21")
+User.create(username: "sokos67", email: "will@comicbooked1.com", password: "laceelou21", password_confirmation: "laceelou21")
+User.create(username: "sokos68", email: "will@comicbooked2.com", password: "laceelou21", password_confirmation: "laceelou21")
+User.create(username: "sokos69", email: "will@comicbooked3.com", password: "laceelou21", password_confirmation: "laceelou21")
+User.create(username: "sokos61", email: "will@comicbooked4.com", password: "laceelou21", password_confirmation: "laceelou21")
+p "Test users created"
