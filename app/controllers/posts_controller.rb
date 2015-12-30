3b9e85fd7bcae6d0052c85f5468df47a8ce43fee
Will Sokolowski <will.sokolowski@gmail.com>
2015-12-29 18:48:10 -0700

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:edit, :update, :destroy]
+	before_action :authenticate_user!, :set_post, only: [:edit, :update, :destroy]
 
 	def create
 		@post = current_user.posts.new(post_params)
